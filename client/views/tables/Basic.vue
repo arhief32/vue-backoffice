6af23ce79926448aa6d99719f40057a11dfbe6ed
Arief Muarrief Buhari <arhief32@gmail.com>
2018-05-18 09:41:14 +0700

    try make with table invuice

--- client/views/tables/Basic.vue
+++ client/views/tables/Basic.vue
@@ -7,289 +7,57 @@
           <table class="table">
             <thead>
               <tr>
-                <th>Name</th>
-                <th>Instrument</th>
-                <th></th>
-                <th></th>
+                <th>ID</th>
+                <th>SLA</th>
+                <th>ID Paket</th>
+                <th>ID Resi</th>
+                <th>Status Paket</th>
+                <th>Asal</th>
+                <th>Tujuan</th>
+                <th>Dikirim pada</th>
+                <th>Pengirim</th>
+                <th>Jumlah</th>
+                <th>Status</th>
               </tr>
             </thead>
-            <tfoot>
-              <tr>
-                <th>Name</th>
-                <th>Instrument</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </tfoot>
             <tbody>
-              <tr>
-                <td>Misty Abbott</td>
-                <td>Bass Guitar</td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
+              <tr v-for="invoice in invoices" :key="invoice.id">
+                <td>
+                  {{ invoice.id }}
                 </td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-instagram"></i>
-                  </a>
+                <td>
+                  {{ invoice.sla }}
                 </td>
-              </tr>
-              <tr>
-                <td>John Smith</td>
-                <td>Rhythm Guitar</td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
+                <td>
+                  {{ invoice.packet_detail.id }}
                 </td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-instagram"></i>
-                  </a>
+                <td>
+                  {{ invoice.packet_detail.object_id }}
                 </td>
-              </tr>
-              <tr>
-                <td>Robert Mikels</td>
-                <td>Lead Guitar</td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
+                <td>
+                  {{ invoice.packet_detail.status }}
                 </td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-instagram"></i>
-                  </a>
+                <td>
+                  {{ invoice.packet_detail.origin_detail.city_name }}
                 </td>
-              </tr>
-              <tr>
-                <td>Karyn Holmberg</td>
-                <td>Drums</td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
+                <td>
+                  {{ invoice.packet_detail.destination_detail.city_name }}
                 </td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-instagram"></i>
-                  </a>
+                <td>
+                  {{ invoice.packet_detail.departure_expectation_time }}
+                </td>
+                <td>
+                  <!-- {{ invoice.packet_detail.sender_address_detail.contact_name }} -->
+                </td>
+                <td>
+                  {{ invoice.amount }}
+                </td>
+                <td>
+                  {{ invoice.status }}
                 </td>
               </tr>
             </tbody>
           </table>
-        </article>
-      </div>
-    </div>
-
-    <div class="tile is-ancestor">
-      <div class="tile is-parent is-4">
-        <article class="tile is-child box">
-          <h4 class="title">Table is-bordered</h4>
-          <table class="table is-bordered">
-            <thead>
-              <tr>
-                <th>One</th>
-                <th>Two</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Three</td>
-                <td>Four</td>
-              </tr>
-            </tbody>
-          </table>
-        </article>
-      </div>
-      <div class="tile is-parent is-4">
-        <article class="tile is-child box">
-          <h4 class="title">Table is-striped</h4>
-          <table class="table is-striped">
-            <thead>
-              <tr>
-                <th>One</th>
-                <th>Two</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Three</td>
-                <td>Four</td>
-              </tr>
-              <tr>
-                <td>Five</td>
-                <td>Six</td>
-              </tr>
-              <tr>
-                <td>Seven</td>
-                <td>Eight</td>
-              </tr>
-            </tbody>
-          </table>
-        </article>
-      </div>
-      <div class="tile is-parent is-4">
-        <article class="tile is-child box">
-          <h4 class="title">Table is-narrow</h4>
-          <table class="table is-narrow">
-            <thead>
-              <tr>
-                <th>One</th>
-                <th>Two</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Three</td>
-                <td>Four</td>
-              </tr>
-              <tr>
-                <td>Five</td>
-                <td>Six</td>
-              </tr>
-              <tr>
-                <td>Seven</td>
-                <td>Eight</td>
-              </tr>
-            </tbody>
-          </table>
-        </article>
-      </div>
-    </div>
-
-    <div class="tile is-ancestor">
-      <div class="tile is-parent">
-        <article class="tile is-child box">
-          <h4 class="title">Table Responsive</h4>
-          <div class="table-responsive">
-            <table class="table is-bordered is-striped is-narrow">
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Open source projects</th>
-                  <th>Year started</th>
-                  <th colspan="3">Links</th>
-                </tr>
-              </thead>
-              <tfoot>
-                <tr>
-                  <th></th>
-                  <th>Open source projects</th>
-                  <th>Year started</th>
-                  <th colspan="3">Links</th>
-                </tr>
-              </tfoot>
-              <tbody>
-                <tr>
-                  <td class="is-icon">
-                    <i class="fa fa-android"></i>
-                  </td>
-                  <td>
-                    <a href="#">Android</a>
-                  </td>
-                  <td>
-                    2003
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-github"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-twitter"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-globe"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="is-icon">
-                    <i class="fa fa-firefox"></i>
-                  </td>
-                  <td>
-                    <a href="#">Firefox</a>
-                  </td>
-                  <td>
-                    2003
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-github"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-twitter"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-globe"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="is-icon">
-                    <i class="fa fa-linux"></i>
-                  </td>
-                  <td>
-                    <a href="#">Linux</a>
-                  </td>
-                  <td>
-                    2003
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-github"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-twitter"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-globe"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="is-icon">
-                    <i class="fa fa-wordpress"></i>
-                  </td>
-                  <td>
-                    <a href="#">WordPress</a>
-                  </td>
-                  <td>
-                    2003
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-github"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-twitter"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-globe"></i>
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
         </article>
       </div>
     </div>
@@ -304,3 +72,26 @@
   overflow-x: auto;
 }
 </style>
+
+<script>
+import axios from 'axios'
+export default {
+  created() {
+    axios.get(`http://localhost:8000/service/invoices/all`)
+    .then(response => {
+      this.invoices = response.data.data.data
+      console.log(this.invoices)
+    })
+    .catch(e => {
+      console.log(e)
+    })
+  },
+
+  data() {
+    return {
+      invoices: [],
+      errors: []
+    }
+  }
+}
+</script>
