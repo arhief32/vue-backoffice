235044c30acda2242ba41d1849fa8bdcffe79f04
Arief Muarrief Buhari <arhief32@gmail.com>
2018-07-05 14:04:37 +0700

    separate component

---
+++ client/views/tables/TableExample.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <article class="tile is-child box">
+          <h4 class="title">Ini Table</h4>
+          <vuetable></vuetable>
+        </article>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import InvoiceTable from './InvoiceTable'
+
+export default {
+  components: {
+    'vuetable': InvoiceTable
+  }
+}
+</script>
